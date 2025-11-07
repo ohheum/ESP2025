2baee0495ad1968e99778c248a500154138b452b
ohheum <ohheum@gmail.com>
2025-11-07 12:55:39 +0900

    Add files via upload

---
+++ chap02/UltraSimpleShell/ush.c
@@ -0,0 +1,30 @@
+
+#include "ush.h"
+#include "makeargv.h"
+#define MAX_BUFFER 256
+
+
+int main (int argc, char *argv[])
+{
+    char inbuf[MAX_BUFFER];
+    char **chargv;
+
+    for(;;) {
+      printf(PROMPT_STRING);
+	   fgets(inbuf, sizeof(inbuf), stdin);
+      if (strlen(inbuf) > 0) inbuf[strlen(inbuf)-1] = '\0';
+	    if (strcmp(inbuf, QUIT_STRING) == 0)
+          break;
+	    else {
+          if (fork() == 0) {
+             if (makeargv(inbuf, BLANK_STRING, &chargv) > 0) {
+                execvp(chargv[0], chargv);
+                printf("%s: command not found.", chargv[0]);
+             }
+             exit(1);
+          }
+          wait(NULL);
+       } 
+    }   
+    return 0;
+}
